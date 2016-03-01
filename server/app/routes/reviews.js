8cb9781808565bd4d6ee11348d32733b4ea7019a
Matt Hsiung <xung@musician.com>
2016-03-01 12:07:26 -0500

    Added reviews route

---
+++ server/app/routes/reviews.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var router = require('express').Router();
+var Reviews = require('mongoose').model('Reviews');
+
+module.exports = router;
+
+//------ GET ALL REVIEWS BY ITEM
+router.get('/user/:userId', function (req, res, next) {
+	Reviews.find({user: req.params.userId})
+	.then(data=>{
+		if(!data.length) res.sendStatus(404);
+		else res.send(data);
+	}, err=>{
+		next(err)
+	})
+});
+
+//------ GET ALL REVIEWS BY USER
+router.get('/item/:itemId', function (req, res, next) {
+	Reviews.find({itemId: req.params.itemId})
+	.then(data=>{
+		if(!data.length) res.sendStatus(404);
+		else res.send(data);
+	}, err=>{
+		next(err)
+	})
+});
+
+//------ CREATE REVIEW
+router.post('/', function(req, res, next){
+	Reviews.create(req.body)
+	.then(data=>{
+		res.status(201).send(data)
+	}, err=>{
+		next(err)
+	})
+});
+
+//------ EDIT REVIEW
+roiuter.put('/:id', function(req, res, next){
+	Reviews.find({_id: req.params.id})
+	.then(data=>{
+		if(!data.length) res.sendStatus(404);
+		else {
+			data.body = 
+			data[0].save()
+			res.send(data[0])
+		}
+	}, err=>{
+		next(err)
+	})
+});
+
+//------ DELETE REVIEW
+roiuter.delete('/:id', function(req, res, next){
+	Reviews.find({_id: req.params.id})
+	.then(data=>{
+		if(!data.length) res.sendStatus(404);
+		else {
+			data[0].remove()
+			res.send(data[0])
+		}
+	}, err=>{
+		next(err)
+	})
+});
