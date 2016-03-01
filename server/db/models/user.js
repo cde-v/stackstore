197a7ce2612d60dd85deb40d7528d01374866988
Chris de Villiers <cpdevill@gmail.com>
2016-03-01 11:02:47 -0500

    Tues AM

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -12,6 +12,8 @@ var userSchema = new mongoose.Schema({
   },
   photoUrl: {
     type: String
+    // default profile pic, no images currently to point to
+    // default: '/images/default-photo.jpg' 
   },
   firstName: {
     type: String
@@ -20,9 +22,6 @@ var userSchema = new mongoose.Schema({
     type: String
   },
   shipAddress: {
-    type: String
-  },
-  billAddress: {
     type: String
   },
   phoneNumber: {
@@ -79,35 +78,43 @@ var userSchema = new mongoose.Schema({
     ref: 'Review'
   }],
   paymentProfiles: [{
-    cc: { type: String, required: true}
+    ccCardholder: { type: String, required: true },
+    ccType: { type: String, required: true },
+    ccNum: { type: String, required: true },
+    ccBillingAddress: { type: String, required: true }
   }]
 });
+
+userSchema.virtual('fullname').get(function() {
+  return this.firstName + " " + this.lastName;
+});
+
+// method to remove sensitive information from user objects before sending them out
+userSchema.methods.sanitize = function() {
+  return _.omit(this.toJSON(), ['password', 'salt']);
+};
+// generateSalt, encryptPassword and the pre 'save' and 'correctPassword' operations
+// are all used for local authentication security.
+var generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
+};
+var encryptPassword = function(plainText, salt) {
+  var hash = crypto.createHash('sha1');
+  hash.update(plainText);
+  hash.update(salt);
+  return hash.digest('hex');
+};
+userSchema.pre('save', function(next) {
+  if(this.isModified('password')) {
+    this.salt = this.constructor.generateSalt();
+    this.password = this.constructor.encryptPassword(this.password, this.salt);
+  }
+  next();
+});
+userSchema.statics.generateSalt = generateSalt;
+userSchema.statics.encryptPassword = encryptPassword;
+userSchema.method('correctPassword', function(candidatePassword) {
+  return encryptPassword(candidatePassword, this.salt) === this.password;
+});
+
 mongoose.model('User', userSchema);
-// // method to remove sensitive information from user objects before sending them out
-// schema.methods.sanitize = function() {
-//   return _.omit(this.toJSON(), ['password', 'salt']);
-// };
-// // generateSalt, encryptPassword and the pre 'save' and 'correctPassword' operations
-// // are all used for local authentication security.
-// var generateSalt = function() {
-//   return crypto.randomBytes(16).toString('base64');
-// };
-// var encryptPassword = function(plainText, salt) {
-//   var hash = crypto.createHash('sha1');
-//   hash.update(plainText);
-//   hash.update(salt);
-//   return hash.digest('hex');
-// };
-// schema.pre('save', function(next) {
-//   if(this.isModified('password')) {
-//     this.salt = this.constructor.generateSalt();
-//     this.password = this.constructor.encryptPassword(this.password, this.salt);
-//   }
-//   next();
-// });
-// schema.statics.generateSalt = generateSalt;
-// schema.statics.encryptPassword = encryptPassword;
-// schema.method('correctPassword', function(candidatePassword) {
-//   return encryptPassword(candidatePassword, this.salt) === this.password;
-// });
-// mongoose.model('User', schema);
