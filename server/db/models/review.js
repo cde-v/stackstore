b31247aa033b2e8c94d2a37861d2e891a8d00831
Chris de Villiers <cpdevill@gmail.com>
2016-03-01 12:37:15 -0500

    user Routes done

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -1,5 +1,4 @@
 'use strict';
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 var db = require('../../db');
 var reviewSchema = new mongoose.Schema({
@@ -21,32 +20,4 @@ var reviewSchema = new mongoose.Schema({
     required: true
   }
 });
-module.exports = db.model('Reviews', reviewSchema);
-=======
-
-var mongoose = require('mongoose');
-
-var db = require('../../db');
-
-var reviewSchema = new mongoose.Schema({
-	product:{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Product'
-	}
-	author: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User'
-	},
-	rating: {
-		type: [Boolean],
-		default:[false,false,false,false,false]
-		required: true
-	},
-	body: {
-		type: String,
-		required: true
-	}
-});
-
-module.exports = db.model('Reviews', reviewSchema);
->>>>>>> master
+db.model('Reviews', reviewSchema);
