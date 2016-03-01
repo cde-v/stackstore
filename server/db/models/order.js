cf37ecee7c7f21a2ef259e90103dc32f2096b355
Cindy Li <il.ydnic@gmail.com>
2016-03-01 12:21:22 -0500

    changes

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,23 +1,23 @@
-var orderSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    cart: {
-       type: mongoose.Schema.Types.ObjectId,
-        ref: 'Cart'
-    },
-    status: {
-        type: String,
-        enum: ['created', 'processing', 'shipped', 'fulfilled', 'canceled', 'error']
-    },
-    returns: {
-        type: String,
-        enum: ['created', 'received', 'canceled', 'refunded']
-    },
-    date: {
-    	type: Date,
-    	default: Date.now
-    }    
-});
+// var orderSchema = new mongoose.Schema({
+//     user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User'
+//     },
+//     cart: {
+//        type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Cart'
+//     },
+//     status: {
+//         type: String,
+//         enum: ['created', 'processing', 'shipped', 'fulfilled', 'canceled', 'error']
+//     },
+//     returns: {
+//         type: String,
+//         enum: ['created', 'received', 'canceled', 'refunded']
+//     },
+//     date: {
+//     	type: Date,
+//     	default: Date.now
+//     }    
+// });
 
