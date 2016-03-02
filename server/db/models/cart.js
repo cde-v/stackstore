0b367d07aed7e10cfcfd34c321a4946f921eb148
Cindy Li <il.ydnic@gmail.com>
2016-03-02 12:15:18 -0500

    cart model

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -2,43 +2,59 @@ var mongoose = require('mongoose');
 
 var cartSchema = new mongoose.Schema({
 
-	user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-	session: {type: String},
-	items: [{product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
-			quantity: {type: Number}
-	}]
-}); 
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  session: { type: String },
+  items: [{
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+    quantity: { type: Number },
+    _id: false
+  }]
+});
 
-cartSchema.methods.checkOut = function(){
-	//route?
-};
+cartSchema.methods.editQuantity = function(id, qty) {
+  var cart = this;
+  var found = false;
 
-cartSchema.methods.editQuantity = function (id, qty) {
-	var cart = this;
+  cart.items.forEach(function(item) {
+    if (item.product._id === id) {
+      item.quantity = qty;
+      found = true;
+    }
+  });
 
-	cart.items.forEach(function(item){
-		if(item.product == id) item.quantity = qty;
-	});
+  if (!found) cart.items.push({ product: id, quantity: qty });
+
+  cart.save();
+
+  return cart;
 };
 
 cartSchema.methods.removeItem = function(id) {
-	var cart = this;
+  var cart = this;
+  var idx = -1;
 
-	cart.items.forEach(function(item, ind){
-		if(item.product == id) cart.items.splice(ind, 1);
-	});
+  cart.items.forEach(function(item, ind) {
+    if (item.product.toString() === id) idx = ind;
+  });
+
+  if (idx >= 0) cart.items.splice(idx, 1);
+  cart.save();
+
+  return cart;
 };
 
-cartSchema.methods.clearCart = function(){
-	//route?
-};
+// cartSchema.methods.getTotal = function() {
+//   var cart = this;
+//   var total = 0;
 
-cartSchema.methods.getTotal = function(){
-	var cart = this;
-	cart.populate('items');
-	return cart.items.reduce(function(prev, curr){
-		return prev + curr.price;
-	}, 0);
-};
+//   cart.populate('items.product').exec();
+
+//   cart.items.forEach(function(item) {
+//     console.log(item);
+//     total += item.product.price;
+//   });
+  
+//   return total;
+// };
 
 mongoose.model('Cart', cartSchema);
