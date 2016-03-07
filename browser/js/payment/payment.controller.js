5680c812ca80e48af449877acc234ba8e29bbd63
Matt Hsiung <xung@musician.com>
2016-03-07 15:15:00 -0500

    stripe init

--- browser/js/payment/payment.controller.js
+++ browser/js/payment/payment.controller.js
@@ -1,3 +1,31 @@
-app.controller("paymentController", function($scope){
+	Stripe.setPublishableKey('pk_test_OAYbGBOJ1PSBsUJBf8We5KHP');
+app.controller("paymentController", function($scope, paymentFactory){
+	$scope.ccn = null
+	$scope.cvc = null
+	$scope.exp = null
+
+	$scope.checkout = function(){
+		var creditCard = {
+			number: $scope.ccn,
+			cvc: $scope.cvc,
+			exp_month: $scope.exp.slice(0, 2),
+			exp_year: $scope.exp.slice(3)
+		}
+
+	  	var info = {
+	  		shipAddress: $scope.shipAddress,
+	  		billAddress: $scope.shipAddress
+  		}
+
+		// console.log(creditCard, info)
+		Stripe.card.createToken(creditCard, function(status, response) {
+			if (response.error) {
+		   		console.log('payment error')
+			} else {
+	  			info.token = response.id
+	  			paymentFactory.checkout(info)
+	  		}
+		})
+	}
 
 });
--- browser/js/payment/payment.factory.js
+++ browser/js/payment/payment.factory.js
@@ -1,5 +1,22 @@
-app.factory('paymentFactory', function(){
-	var payment = {};
+app.factory('paymentFactory', function($http, $state, CartFactory, AuthService, $rootScope){
+	
+	var payment = {
+
+		checkout: function(info){
+			AuthService.getLoggedInUser()
+			.then(function(user){
+				if(user){
+					return $http.post('checkout/'+user.cart, info)
+					.then(function(data){
+						//payment success
+						$state.go('home')
+
+					}, function(err){console.log(err)})
+				}
+			})
+		}
+
+	};
 
 	
 
