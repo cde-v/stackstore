b67a18ef28a7da3fd5653622210c4bbf24a4e0ab
Chris de Villiers <cpdevill@gmail.com>
2016-03-08 10:40:56 -0500

    refactored a bit

--- browser/js/admin/admin.users.list.js
+++ browser/js/admin/admin.users.list.js
@@ -29,34 +29,43 @@ app.controller('adminUserCtrl', function($scope, $state, users, UserList) {
 });
 
 app.factory('UserList', function($http, $state) {
-
-  return {
-    getAll: function() {
-      return $http.get('/api/users/')
-        .then(function(users) {
-          return users.data;
-        });
-    },
-    destroy: function(user) {
-      return $http.delete('/api/users/' + user._id)
-        .then(function(user) {
-          $state.reload();
-          return user.data;
-        });
-    },
-    toggleAdmin: function(user) {
-      return $http.put('/api/users/toggleAdmin/' + user._id)
-        .then(function(product) {
-          $state.reload();
-          return product.data;
-        });
-    },
-    toggleNeedsPasswordReset: function(user) {
-      return $http.put('/api/users/toggleNeedsPasswordReset/' + user._id)
-        .then(function(product) {
-          $state.reload();
-          return product.data;
-        });
-    }
+  var factory = {
+    getAll: getAll,
+    destroy: destroy,
+    toggleAdmin: toggleAdmin,
+    toggleNeedsPasswordReset: toggleNeedsPasswordReset
   };
+  return factory;
+
+  function getAll() {
+    return $http.get('/api/users/')
+      .then(function(users) {
+        return users.data;
+      });
+  }
+
+  function destroy(user) {
+    return $http.delete('/api/users/' + user._id)
+      .then(function(user) {
+        $state.reload();
+        return user.data;
+      });
+  }
+
+  function toggleAdmin(user) {
+    return $http.put('/api/users/toggleAdmin/' + user._id)
+      .then(function(product) {
+        $state.reload();
+        return product.data;
+      });
+  }
+
+  function toggleNeedsPasswordReset(user) {
+    return $http.put('/api/users/toggleNeedsPasswordReset/' + user._id)
+      .then(function(product) {
+        $state.reload();
+        return product.data;
+      });
+  }
+
 });
