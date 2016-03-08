1e9e8f5ab0e48912e250d162b489e3b24ce95216
oceanwaves19 <Khadija@Khadijas-MacBook-Pro.local>
2016-03-08 11:00:31 -0500

    admin navigation completed

--- browser/js/products/products.detail.js
+++ browser/js/products/products.detail.js
@@ -14,15 +14,63 @@ app.config(function ($stateProvider) {
     });
 });
 
+app.controller('productDetailCtrl', function ($scope, shoe, AuthService, ProductList, $state, CartFactory, reviews, ReviewFactory) {
+	reviews.forEach(function(review){
+			review.stars = [];
+			for (var i = 0; i < review.rating; i++) {
+				review.stars.push(true)
+			}
+	})
 
-app.controller('productDetailCtrl', function ($scope, shoe, ProductList, $state, CartFactory, reviews) {
 	$scope.reviews = reviews;
+	$scope.rating = null;
+	$scope.body = '';
+
+	$scope.setRating=function(rating){
+		$scope.rating = rating;
+		$scope.product = shoe._id
+		 AuthService.getLoggedInUser()
+		 .then(user=> $scope.author=user._id);
+
+	}
+
+	$scope.writeReview = function(){
+		if($scope.author && $scope.rating && $scope.body){
+			ReviewFactory.add($scope.author, $scope.rating, $scope.body, $scope.product)
+		}
+	}
+
+
 	$scope.shoe = shoe;
 	$scope.sizes = ProductList.getAvailSizes($scope.shoe);
 	$scope.selSize = 'Size';
 	$scope.selectSize = function(size){
 		$scope.selSize = size;
 	}
+
 	$scope.addItem = CartFactory.addItem;
+
+
 })
 
+app.factory('ReviewFactory', function ($http) {
+	return {
+		add:function(author, rating, body, product){
+			var data = {
+				author:author,
+				rating:rating,
+				body:body,
+				product:product
+			}
+			console.log(data)
+			return $http.post('api/reviews', data)
+		}
+	};
+})
+
+app.directive('review', function(){
+	return {
+    	restrict: 'E',
+    	templateUrl: 'js/products/review.html'
+	}
+})
